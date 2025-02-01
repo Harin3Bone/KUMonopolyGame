3e0b8f3a88c75046ff7b4d68de722b3855bd19d3
Harin3Bone <harin.thananam@gmail.com>
2025-02-01 14:14:08 +0700

    Implement MGame, Board and Die

--- src/main/java/monopoly/entity/MGame.java
+++ src/main/java/monopoly/entity/MGame.java
@@ -1,20 +1,75 @@
 package main.java.monopoly.entity;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MGame {
 
     private int roundCnt;
+    private int lastRound;
     private Board board;
-    private Die dice;
     private List<Player> players;
 
-    public MGame(int roundCnt) {
+    private static final List<Die> DICES = new ArrayList<>(List.of(new Die(), new Die()));
+    private static final int MIN_PLAYERS = 2;
+    private static final int MAX_PLAYERS = 8;
+
+    public MGame() {
+        // Set default round start at 1
+        this.roundCnt = 1;
+
+        // Set default final round at 20
+        this.lastRound = 10;
+
+        // Initial board setup
+        this.board = new Board();
+        this.board.init();
+        this.players = new ArrayList<>();
+    }
+
+    public void addPlayer(Player... players) {
+        if ((this.players.size() + players.length) > MAX_PLAYERS) {
+            throw new IllegalArgumentException("The number of players must be less than or equal to %s".formatted(MAX_PLAYERS));
+        }
+
+        this.players.addAll(List.of(players));
+    }
+
+    public int getRoundCnt() {
+        return roundCnt;
+    }
+
+    public void setRoundCnt(int roundCnt) {
         this.roundCnt = roundCnt;
     }
 
-    public void playGame() {}
+    public int getLastRound() {
+        return lastRound;
+    }
 
-    private void playRound() {}
+    public void setLastRound(int lastRound) {
+        this.lastRound = lastRound;
+    }
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public void setBoard(Board board) {
+        this.board = board;
+    }
+
+    public void playGame() {
+        while (this.roundCnt <= this.lastRound) {
+            playRound();
+            roundCnt++;
+        }
+    }
+
+    private void playRound() {
+        for (var player : players) {
+            player.takeTurn(board, DICES);
+        }
+    }
 
 }
